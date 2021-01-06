36ec7dd14c5eac9b693625fa098a6211ff6f23aa
lisa nalyvaiko <73755078+lisa-nalyvaiko@users.noreply.github.com>
2021-01-06 14:35:54 +0200

    update

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-from dotenv import load_dotenv
-load_dotenv()
 
 sa = SpreadsheetApp(from_env=True)
 table_name = 'Invoicing Statistics 2020'
